9fc8f6327475b2387bd15ecb4688e409bbe0c9c9
Jeff Trempe <trempe.jeff@gmail.com>
2020-02-01 15:46:53 -0500

    Created Doses controller and Cocktails controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # cocktail new, create, index, show
+  # doses new, create, destroy
+  # ingredients new, create, destroy
+  resources :cocktails, only: %i[new create index show] do
+    resources :doses, only: %i[new create destroy]
+  end
+
+  resources :ingredients, only: %i[new create index]
 end
